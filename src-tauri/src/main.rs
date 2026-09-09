ec2e11c56d39eaaae4588214266588ec715a967c
Salah Abdulrahman <salah707@gmail.com>
2026-09-09 23:39:15 +0300

    Format main.rs for better readability

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,18 +3,18 @@
 use tauri::{Manager, CustomMenuItem, SystemTray, SystemTrayEvent, SystemTrayMenu};
 
 fn main() {
-  tauri::Builder::default()
-    .invoke_handler(tauri::generate_handler![ping, app_info])
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![ping, app_info])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
 
 #[tauri::command]
 fn ping() -> String {
-  "pong".into()
+    "pong".into()
 }
 
 #[tauri::command]
 fn app_info() -> String {
-  format!("Veltrix Tauri backend (stub)")
+    format!("Veltrix Tauri backend (stub)")
 }
